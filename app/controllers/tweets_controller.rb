13d782ee195dce737359104a340fec3063bac6d9
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-09-16 01:05:25 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,70 @@
 class TweetsController < ApplicationController
+enable :sessions
+  set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
 
+  get '/tweets' do
+    if !!session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+      @tweets = Tweet.all
+      erb :"/tweets/index"
+    else 
+      redirect to '/login'
+    end 
+  end
+
+  get '/tweets/new' do
+    if !!session[:user_id]
+      erb :"/tweets/new"
+    else 
+      redirect to "/login"
+    end 
+  end
+
+  post '/tweets' do
+    if params[:content].empty?
+      redirect to "/tweets/new"
+    else
+      User.find_by(id: session[:user_id]).tweets.create(content: params[:content])
+    end 
+  end
+
+  get '/tweets/:id' do
+    if !!session[:user_id]
+      
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :"/tweets/show"
+    else 
+      redirect to "/login"
+    end 
+  end
+
+  get '/tweets/:id/edit' do
+    if !!session[:user_id] && @tweet = User.find_by(id: session[:user_id]).tweets.find_by(id: params[:id])
+      erb :"/tweets/edit"
+    else 
+      redirect to "/login"
+    end
+  end
+
+  patch '/tweets/:id' do
+    tweet = Tweet.find_by(id: params[:id])
+    if params[:content].empty?
+      redirect to "/tweets/#{tweet.id}/edit"
+    else 
+      tweet.update(content: params[:content])
+      tweet.save
+      redirect to "/tweets/#{tweet.id}"
+    end 
+  end
+
+  delete '/tweets/:id' do
+    tweet = Tweet.find_by(id: params[:id])
+    if !!session[:user_id] && tweet.user.id == session[:user_id]
+      tweet.delete 
+      redirect to "/tweets"
+    else
+      redirect to '/login'
+    end 
+  end 
 
 end
